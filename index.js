fe8539df947cc0740791e35a9a3792e13c6e0d14
AJ Guiles <ashiguiles@gmail.com>
2019-10-22 02:22:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
-// Write your solution in this file!
+// // Write your solution in this file!
 
-// let driver = {oldMan: "jeremy",smarOne: "james", shortOne: "richard"}
-let  driver = {};
+// // let driver = {oldMan: "jeremy",smarOne: "james", shortOne: "richard"}
+// let  driver = {};
 
-function updateDriverWithKeyAndValue()(driver,key, value) {
-//   const newDriver = {...driver};
-//   newDriver[quiteOne] = "stig";
-//   return newDriver;
+// function updateDriverWithKeyAndValue()(driver,key, value) {
+// //   const newDriver = {...driver};
+// //   newDriver[quiteOne] = "stig";
+// //   return newDriver;
 
-  // return Object.assign(driver, { quiteOne:"stig"} ); //this is distructive 
-  // return Object.assign({}, driver, { quiteOne:"stig"} );  this is non-distructive 
-  return Object.assign({}, driver, { [key]: value });
-}
+//   // return Object.assign(driver, { quiteOne:"stig"} ); //this is distructive 
+//   // return Object.assign({}, driver, { quiteOne:"stig"} );  this is non-distructive 
+//   return Object.assign({}, driver, { [key]: value });
+// }
 
-function destructivelyUpdateDriverWithKeyAndValue(driver,object, key) {
-  // driver[oldMan] = "joe";
-  // return driver
-  driver[key] = value;
-  return driver;
+// function destructivelyUpdateDriverWithKeyAndValue(driver,object, key) {
+//   // driver[oldMan] = "joe";
+//   // return driver
+//   driver[key] = value;
+//   return driver;
 
-}
+// }
 
 
-function deleteFromDriverBykey(driver,object, key)  {
-  // const newDriver = Object.assign({}, driver);
-  // delete newDriver[oldMan];
-  // return newDriver;
+// function deleteFromDriverBykey(driver,object, key)  {
+//   // const newDriver = Object.assign({}, driver);
+//   // delete newDriver[oldMan];
+//   // return newDriver;
 
-  const newObj = Object.assign({}, driver);
-  delete newObj[key];
-  return newObj
-}
+//   const newObj = Object.assign({}, driver);
+//   delete newObj[key];
+//   return newObj
+// }
 
-function destructivelyDeleteFromDriverByKey(driver,object, key) {
-  // delete driver[oldMan];
-  // return driver;
-  delete driver[key];
-  return driver;
+// function destructivelyDeleteFromDriverByKey(driver,object, key) {
+//   // delete driver[oldMan];
+//   // return driver;
+//   delete driver[key];
+//   return driver;
   
-}
+// }
 
 
