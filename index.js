5e4f5de109e1d34e216d8c79a8d3d5c2cc8c1368
AJ Guiles <ashiguiles@gmail.com>
2019-10-22 02:17:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Write your solution in this file!
 
-let driver = {oldMan: "jeremy",smarOne: "james", shortOne: "richard"}
+// let driver = {oldMan: "jeremy",smarOne: "james", shortOne: "richard"}
+let  driver = {};
 
 function updateDriveWithKeyAndValue(driver,key, value) {
 //   const newDriver = {...driver};
@@ -12,21 +13,30 @@ function updateDriveWithKeyAndValue(driver,key, value) {
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver,object, key) {
-  driver[oldMan] = "joe";
-  return driver
+  // driver[oldMan] = "joe";
+  // return driver
+  driver[key] = value;
+  return driver;
 
 }
 
 
 function deleteFromDriverBykey(driver,object, key)  {
-  const newDriver = Object.assign({}, driver);
-  delete newDriver[oldMan];
-  return newDriver;
+  // const newDriver = Object.assign({}, driver);
+  // delete newDriver[oldMan];
+  // return newDriver;
+
+  const newObj = Object.assign({}, driver);
+  delete newObj[key];
+  return newObj
 }
 
 function destructivelyDeleteFromDriverByKey(driver,object, key) {
+  // delete driver[oldMan];
+  // return driver;
   delete driver[key];
   return driver;
   
 }
+
 
