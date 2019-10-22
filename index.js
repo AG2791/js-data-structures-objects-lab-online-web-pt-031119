4a72680b79682d9d4632b9704e5e56b408ce470f
AJ Guiles <ashiguiles@gmail.com>
2019-10-22 02:28:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,73 +1,41 @@
-// // Write your solution in this file!
+// Write your solution in this file!
+// let driver = {oldMan: "jeremy",smarOne: "james", shortOne: "richard"}
+let  driver = {};
 
-// // let driver = {oldMan: "jeremy",smarOne: "james", shortOne: "richard"}
-// let  driver = {};
+function updateDriveWithKeyAndValue(driver,key, value) {
+//   const newDriver = {...driver};
+//   newDriver[quiteOne] = "stig";
+//   return newDriver;
 
-// function updateDriverWithKeyAndValue()(driver,key, value) {
-// //   const newDriver = {...driver};
-// //   newDriver[quiteOne] = "stig";
-// //   return newDriver;
-
-//   // return Object.assign(driver, { quiteOne:"stig"} ); //this is distructive 
-//   // return Object.assign({}, driver, { quiteOne:"stig"} );  this is non-distructive 
-//   return Object.assign({}, driver, { [key]: value });
-// }
-
-// function destructivelyUpdateDriverWithKeyAndValue(driver,object, key) {
-//   // driver[oldMan] = "joe";
-//   // return driver
-//   driver[key] = value;
-//   return driver;
-
-// }
-
-
-// function deleteFromDriverBykey(driver,object, key)  {
-//   // const newDriver = Object.assign({}, driver);
-//   // delete newDriver[oldMan];
-//   // return newDriver;
-
-//   const newObj = Object.assign({}, driver);
-//   delete newObj[key];
-//   return newObj
-// }
-
-// function destructivelyDeleteFromDriverByKey(driver,object, key) {
-//   // delete driver[oldMan];
-//   // return driver;
-//   delete driver[key];
-//   return driver;
-  
-// }
-
-
-const driver = {};
-
-function updateDriverWithKeyAndValue(driver, key, value) {
-  // Alternate using ES6 Spread operators:
-  // return { ...driver, ...{ [key]: value } }
-  return Object.assign({}, driver, { [key]: value });
+return Object.assign({}, driver, { [key]: value });
+  // return Object.assign(driver, { quiteOne:"stig"} ); //this is distructive 
+  // return Object.assign({}, driver, { quiteOne:"stig"} );  this is non-distructive 
 }
 
-function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+function destructivelyUpdateDriverWithKeyAndValue(driver,key, value) {
+  // driver[oldMan] = "joe";
+  // return driver
   driver[key] = value;
-
   return driver;
+
 }
 
-function deleteFromDriverByKey(driver, key) {
-  // Alternate using ES6 Spread operators:
-  // const newObj = { ...driver }
+
+function deleteFromDriverBykey(driver, key)  {
+  // const newDriver = Object.assign({}, driver);
+  // delete newDriver[oldMan];
+  // return newDriver;
   const newObj = Object.assign({}, driver);
-
   delete newObj[key];
-
   return newObj;
 }
 
 function destructivelyDeleteFromDriverByKey(driver, key) {
+  // delete driver[oldMan];
+  // return driver;
   delete driver[key];
-
   return driver;
+  
 }
+
 
